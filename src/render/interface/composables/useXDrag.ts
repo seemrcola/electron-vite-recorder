4c01c6c5f7b1f005e2ff184fdadaca99fcbb3618
seemrcola <seemrcola@163.com>
2024-03-10 22:36:30 +0800

    wip: clip ruler

--- src/render/interface/composables/useXDrag.ts
+++ src/render/interface/composables/useXDrag.ts
@@ -1,7 +1,11 @@
 import { onUnmounted, ref } from 'vue'
 
-export function useXDrag(dom: HTMLElement) {
-  let lock = false
+interface Options {
+  border: number
+}
+
+export function useXDrag(dom: HTMLElement, options: Options) {
+  const lock = ref(false)
   const startX = ref(0)
 
   function start() {
@@ -9,7 +13,7 @@ export function useXDrag(dom: HTMLElement) {
   }
 
   function onmousedown(e: MouseEvent) {
-    lock = true
+    lock.value = true
     startX.value = e.clientX
 
     document.addEventListener('mousemove', onmousemove)
@@ -17,20 +21,25 @@ export function useXDrag(dom: HTMLElement) {
   }
 
   function onmousemove(e: MouseEvent) {
-    if (lock) {
+    if (lock.value) {
       // 计算移动的距离
       const distance = e.clientX - startX.value
       // 获取之前的位置
       const left = Number.parseInt(dom.style.left || '0')
       // 设置新的位置
-      dom.style.left = `${left + distance}px`
+      let newX = left + distance
+      if (newX < 0)
+        newX = 0
+      if (newX > options.border)
+        newX = options.border
+      dom.style.left = `${newX}px`
       // 更新起始位置
       startX.value = e.clientX
     }
   }
 
   function onmouseup() {
-    lock = false
+    lock.value = false
     document.removeEventListener('mousemove', onmousemove)
     document.removeEventListener('mouseup', onmouseup)
   }
@@ -46,6 +55,7 @@ export function useXDrag(dom: HTMLElement) {
   start()
 
   return {
+    lock,
     start,
     destroy,
     x: startX,
--- src/render/interface/pages/clip/components/player.vue
+++ src/render/interface/pages/clip/components/player.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useXDrag } from '../../../composables/useXDrag'
 
 const props = defineProps<{
@@ -19,19 +19,41 @@ const leftRef = ref<HTMLDivElement | null>(null)
 const rightRef = ref<HTMLDivElement | null>(null)
 const rulerWidth = ref(0)
 
+const leftHookResult = ref<any>({})
+const rightHookResult = ref<any>({})
+
+const coverWidth = ref(0)
+const coverLeft = ref(0)
+
+watch(
+  () => [leftHookResult.value.x, rightHookResult.value.x], // 只要x变了代表在移动
+  () => {
+    // 先获取尺子到的clientX
+    const rulerClientX = rulerRef.value!.getBoundingClientRect().left
+    // 获取leftRef的clientX
+    const leftClientX = leftRef.value!.getBoundingClientRect().left
+    // 获取rightRef的clientX
+    const rightClientX = rightRef.value!.getBoundingClientRect().left
+
+    // 计算cover的宽度和left
+    coverWidth.value = Math.abs(rightClientX - leftClientX)
+    coverLeft.value = Math.min(leftClientX, rightClientX) - rulerClientX
+  },
+)
+
 function initRuler() {
   const ruler = rulerRef.value!
   rulerWidth.value = ruler.clientWidth
 
   // 左侧右侧的先定位
-  leftRef.value!.style.left = '0'
-  rightRef.value!.style.left = `${rulerWidth.value - 1}px`
+  leftRef.value!.style.left = '-4px'
+  rightRef.value!.style.left = `${rulerWidth.value}px`
 
   // 左侧右侧的拖动
   const left = leftRef.value!
   const right = rightRef.value!
-  useXDrag(left)
-  useXDrag(right)
+  leftHookResult.value = useXDrag(left, { border: rulerWidth.value - 4 })
+  rightHookResult.value = useXDrag(right, { border: rulerWidth.value - 4 })
 }
 
 onMounted(() => {
@@ -50,12 +72,16 @@ onMounted(() => {
     />
     <div ref="rulerRef" w="90%" b="0.25rem solid dark" m-auto h-50px my-4 relative>
       <div
-        ref="leftRef" h-full w-1 bg-blue
-        absolute cursor-pointer
+        ref="leftRef" h-full w="4px" bg-blue
+        absolute z-9 cursor-pointer
       />
       <div
-        ref="rightRef" h-full w-1 bg-red
-        absolute cursor-pointer
+        ref="rightRef" h-full w="4px" bg-red
+        absolute z-9 cursor-pointer
+      />
+      <div
+        absolute z-1 bg-red-1 h-full
+        :style="{ left: `${coverLeft}px`, width: `${coverWidth}px` }"
       />
     </div>
   </div>
